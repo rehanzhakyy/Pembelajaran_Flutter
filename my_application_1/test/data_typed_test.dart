e2017ea6beed09808bd11f4c4375ed8ef29d41ea
rehanzhakyy <hannn9491@gmail.com>
2024-07-29 16:00:16 +0700

    Initial Commit

---
+++ my_application_1/test/data_typed_test.dart
@@ -0,0 +1,41 @@
+void main() {
+  // String
+  // Digunakan untuk menampung nilai String
+  String keyboard = "Rexus";
+  print(keyboard);
+
+  // Bool
+  // Digunakan untuk menampung nilai Boolean True / False
+  bool isMechanical = true;
+  print("Apakah Rexus termasuk keyboard mekanikal? $isMechanical");
+
+  // Num
+  // Digunakan untuk menampung nilai angka bilangan bulat bahkan desimal
+  num percent = 65;
+  print(percent);
+
+  // Double
+  // Digunakan untuk menampung nilai desimal
+  double umur = 23.3;
+  print(umur);
+
+  // Int
+  // Digunakan untuk menampung nilai bilangan bulat
+  int nomorPunggung = 16;
+  print(nomorPunggung);
+
+  // List
+  // Digunakan untuk menampung List
+  List<String> mechKeyboard = ['Rexus', 'Red Dragon', 'Noir'];
+  print(mechKeyboard);
+
+  // Dynamic
+  // Digunakan untuk menampung list dengan berbagai tipe data
+  List<dynamic> favKeyboard = ['Noir', 'Mecha Dragon', 'Rexus'];
+  print(favKeyboard);
+
+  // Map
+  // Digunakan untuk menampung nilai seperti format Json
+  Map laptop = {"biasa": "Asus Vivobook", "gaming": "Asus TUF Gaming"};
+  print(laptop);
+}
---
+++ my_application_1/test/widget_test.dart
@@ -0,0 +1,15 @@
+void main() {
+  // Statically Typed
+  // Wajib mendefinisikan variabel sebelum digunakan
+  var nama = "Raihan Zhaky";
+  print(nama);
+  // Type Inference
+  // Tidak perlu menjelaskan tipe data nya lagi
+  var umur = 23;
+  print(umur);
+  // String Interpolation
+  // Sama kayak String Templates
+  print("Nama saya adalah $nama dan umur saya adalah $umur");
+
+  
+}
