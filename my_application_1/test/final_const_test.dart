9bb3e0d96f27bf993d99814bbcefcaf53507e18c
rehanzhakyy <hannn9491@gmail.com>
2024-07-29 16:12:26 +0700

    menambahkan pembelajaran final dan const

---
+++ my_application_1/test/final_const_test.dart
@@ -0,0 +1,14 @@
+void main() {
+  // Final
+  // Nilai tidak bisa diubah ketika sudah diinisialisasikan
+  // Nilai bisa ditaro belakangan
+  final String name;
+  name = "Zhaky";
+  print(name);
+
+  // Const
+  // Nilai tidak bisa diubah ketika sudah diinisialisasikan
+  // Nilai tidak bisa ditaro belakangan
+  const String address = "Jalan Pengibu";
+  print(address);
+}
