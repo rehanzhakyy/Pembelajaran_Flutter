d85db96f43831a43ee374c67e0483ddd01d8a593
rehanzhakyy <hannn9491@gmail.com>
2024-07-29 17:37:51 +0700

    menambahkan pembelajarn comments

---
+++ my_application_1/test/comment_test.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Single Line Comment
+  // Hanya bisa komen untuk satu baris
+
+  /* Multiline Comment */
+  // Bisa untuk banyak baris komen
+
+  /// Documentation Comment [widget_test_dart]
+  // Untuk dokumentasi comment
+}
