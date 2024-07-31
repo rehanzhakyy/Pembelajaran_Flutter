8c7342df8fa4f5528f3228391dd442c5a83905d1
ree <hannn9491@gmail.com>
2024-07-31 14:04:52 +0700

    menambahkan pembelajaran loop

---
+++ my_application_1/test/07_loop_test.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Ini adalah kasus jika kita menggunakan for
+  for (int i = 1; i <= 10; i++) {
+    print("Urutan Nomer yang benar adalah $i");
+  }
+
+//  Ini adalah kasus jika menggunakan do while
+  int i = 0;
+  do{
+    print(i);
+    i++;
+  } while (i <= 10);
+}
